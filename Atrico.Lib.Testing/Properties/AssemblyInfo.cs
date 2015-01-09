cdbb033708b228fa7ecb248c509244f7f4512d14
AtricoSoftware <developer@atrico.net>
2015-01-09 20:57:43 +0000

    Re-added missing class (TestPODTypes)

--- Atrico.Lib.Testing/Properties/AssemblyInfo.cs
+++ Atrico.Lib.Testing/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@
 [assembly: AssemblyTitle("Atrico.Lib.Testing")]
 [assembly: AssemblyProduct("Atrico.Lib.Testing")]
 [assembly: AssemblyDescription("Testing utilites")]
-[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
