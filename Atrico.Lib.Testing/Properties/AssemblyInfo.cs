4ac77579635c5587be4998f9b0fcc8c4cf968a39
Rob Rimmer <developer@atrico.net>
2015-05-25 19:48:47 +0100

    Atrico libs updated

--- Atrico.Lib.Testing/Properties/AssemblyInfo.cs
+++ Atrico.Lib.Testing/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@
 [assembly: AssemblyTitle("Atrico.Lib.Testing")]
 [assembly: AssemblyProduct("Atrico.Lib.Testing")]
 [assembly: AssemblyDescription("Testing utilites")]
-[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyVersion("1.3.2.0")]
