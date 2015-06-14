377199cfab8387364e7c95eb972a824ee12e4b4e
Rob Rimmer <developer@atrico.net>
2015-06-14 20:57:22 +0100

    Missing ctor

--- Atrico.Lib.Testing/NUnitAttributes/RangeAttribute.cs
+++ Atrico.Lib.Testing/NUnitAttributes/RangeAttribute.cs
@@ -2,5 +2,29 @@
 {
     public class RangeAttribute : NUnit.Framework.RangeAttribute
     {
+        public RangeAttribute(int @from, int to)
+            : base(@from, to)
+        {
+        }
+
+        public RangeAttribute(int @from, int to, int step)
+            : base(@from, to, step)
+        {
+        }
+
+        public RangeAttribute(long @from, long to, long step)
+            : base(@from, to, step)
+        {
+        }
+
+        public RangeAttribute(double @from, double to, double step)
+            : base(@from, to, step)
+        {
+        }
+
+        public RangeAttribute(float @from, float to, float step)
+            : base(@from, to, step)
+        {
+        }
     }
 }
