5bc822cde5ad1686acbd47895f68416fb8953fbb
Rob Rimmer <developer@atrico.net>
2015-07-09 15:48:12 +0100

    RunContextInfo added to TestFixtureBase

--- Atrico.Lib.Testing/TestFixtureBase.cs
+++ Atrico.Lib.Testing/TestFixtureBase.cs
@@ -1,10 +1,31 @@
-﻿namespace Atrico.Lib.Testing
+﻿using System;
+using Atrico.Lib.Common;
+using Moq;
+
+namespace Atrico.Lib.Testing
 {
     public abstract class TestFixtureBase
     {
+        private readonly Lazy<IRunContextInfo> _runContextInfo = new Lazy<IRunContextInfo>(CreateRunContextInfo);
         /// <summary>
         ///     Random value generator
         /// </summary>
         protected readonly RandomValueGenerator RandomValues = new RandomValueGenerator();
+
+
+        protected IRunContextInfo MockRunContext
+        {
+            get { return _runContextInfo.Value; }
+        }
+
+        private static IRunContextInfo CreateRunContextInfo()
+        {
+            var mock = new Mock<IRunContextInfo>();
+            mock.Setup(rc => rc.EntryAssemblyName).Returns(@"assembly.name");
+            mock.Setup(rc => rc.EntryAssemblyVersion).Returns(new Version(1, 2, 3, 4));
+            mock.Setup(rc => rc.EntryAssemblyCopyright).Returns(@"copyright");
+            mock.Setup(rc => rc.EntryAssemblyPath).Returns(@"C:\test.exe");
+            return mock.Object;
+        }
     }
 }
